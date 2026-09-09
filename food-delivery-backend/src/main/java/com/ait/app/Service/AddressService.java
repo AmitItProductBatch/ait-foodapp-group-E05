430466472c6e5e1b5be88f0500979b97221c2e2d
Saurabh More <saurabhmore2074@gmail.com>
2026-09-09 15:48:10 +0530

    ofd
    done

--- food-delivery-backend/src/main/java/com/ait/app/Service/AddressService.java
+++ food-delivery-backend/src/main/java/com/ait/app/Service/AddressService.java
@@ -5,11 +5,10 @@ import java.util.List;
 import com.ait.app.requestbody.AddressDto;
 
 public interface AddressService {
-	
-	AddressDto createAddress(AddressDto addressDto);
+
+AddressDto createAddress(AddressDto addressDto);
 	
 	List<AddressDto> getAllAddresses();
 	
 	AddressDto getAddressById(int id);
-
 }
