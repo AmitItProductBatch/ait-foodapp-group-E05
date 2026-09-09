d27e96b6b952eee15729eed43403acbc2ab4b98e
Saurabh More <saurabhmore2074@gmail.com>
2026-09-09 19:07:11 +0530

    ONL102
    Update

--- food-delivery-backend/src/main/java/com/ait/app/service/UserService.java
+++ food-delivery-backend/src/main/java/com/ait/app/service/UserService.java
@@ -1,4 +1,4 @@
-package com.ait.app.Service;
+package com.ait.app.service;
 
 
 import com.ait.app.requestbody.UserDTO;
--- food-delivery-backend/src/main/java/com/ait/app/serviceimpl/AddressServiceImpl.java
+++ food-delivery-backend/src/main/java/com/ait/app/serviceimpl/AddressServiceImpl.java
@@ -1,12 +1,13 @@
+
 package com.ait.app.serviceimpl;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ait.app.Service.AddressService;
+import com.ait.app.service.AddressService;
 import com.ait.app.model.Address;
 import com.ait.app.repository.AddressRepo;
 import com.ait.app.requestbody.AddressDto;
@@ -14,53 +15,64 @@ import com.ait.app.requestbody.AddressDto;
 @Service
 public class AddressServiceImpl implements AddressService {
 
-	@Autowired
-	private AddressRepo addressRepo;
-	
-	@Override
-	public AddressDto createAddress(AddressDto addressDto) {
-		
-		Address address = new Address();
-		address.setHouseNo(addressDto.getHouseNo());
-		address.setStreetName(addressDto.getStreetName());
-		address.setLandmark(addressDto.getLandmark());
-		address.setCity(addressDto.getCity());
-		address.setPinCode(addressDto.getPincode());
-		
-		Address saved = addressRepo.save(address);
-		return toDto(saved);
-	}
-	
+    @Autowired
+    private AddressRepo addressRepo;
 
-	@Override
-	public List<AddressDto> getAllAddresses() {
-		
-		return addressRepo.findAll()
-				.stream()
-				.map(this::toDto)
-				.collect(Collectors.toList());
-	}
+    @Override
+    public AddressDto createAddress(AddressDto addressDto) {
 
-	@Override
-	public AddressDto getAddressById(int id) {
-		Address address = addressRepo.findById(id).orElse(null);
-		return toDto(address);
-	}
-	
-	private AddressDto toDto(Address address) {
-		if(address == null) {
-			return null;
-		}
-		
-		AddressDto dto = new AddressDto();
-		dto.setId(address.getId());
-		dto.setHouseNo(address.getHouseNo());
-		dto.setStreetName(address.getStreetName());
-		dto.setLandmark(address.getLandmark());
-		dto.setCity(address.getCity());
-		dto.setPincode(address.getPinCode());
-		return dto;
-	}
+        Address address = new Address();
 
-	
+        address.setHouseNo(addressDto.getHouseNo());
+        address.setStreetName(addressDto.getStreetName());
+        address.setLandmark(addressDto.getLandmark());
+        address.setCity(addressDto.getCity());
+        address.setPinCode(addressDto.getPincode());
+
+        Address saved = addressRepo.save(address);
+
+        return toDto(saved);
+    }
+
+    @Override
+    public List<AddressDto> getAllAddresses() {
+
+        List<Address> addresses = addressRepo.findAll();
+
+        List<AddressDto> addressDtoList = new ArrayList<AddressDto>();
+
+        for (Address address : addresses) {
+            AddressDto dto = toDto(address);
+            addressDtoList.add(dto);
+        }
+
+        return addressDtoList;
+    }
+
+    @Override
+    public AddressDto getAddressById(int id) {
+
+        Address address = addressRepo.findById(id).orElse(null);
+
+        return toDto(address);
+    }
+
+    private AddressDto toDto(Address address) {
+
+        if (address == null) {
+            return null;
+        }
+
+        AddressDto dto = new AddressDto();
+
+        dto.setId(address.getId());
+        dto.setHouseNo(address.getHouseNo());
+        dto.setStreetName(address.getStreetName());
+        dto.setLandmark(address.getLandmark());
+        dto.setCity(address.getCity());
+        dto.setPincode(address.getPinCode());
+
+        return dto;
+    }
 }
+
