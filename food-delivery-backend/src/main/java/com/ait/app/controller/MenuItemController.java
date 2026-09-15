f0b45f12c7070d76768db5aa85c8937e392e51e4
akashkadam11 <kadamakash1104@gmail.com>
2026-09-15 20:11:58 +0530

    added view menu

--- food-delivery-backend/src/main/java/com/ait/app/controller/MenuItemController.java
+++ food-delivery-backend/src/main/java/com/ait/app/controller/MenuItemController.java
@@ -1,8 +1,11 @@
 package com.ait.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,39 +14,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ait.app.model.Restaurant;
 import com.ait.app.repository.RestaurantRepository;
+import com.ait.app.requestbody.CategoryMenuDto;
 import com.ait.app.requestbody.MenuItemRequestDTO;
 import com.ait.app.requestbody.MenuItemResponseDTO;
 import com.ait.app.service.MenuItemService;
-
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/MenuItems")
 public class MenuItemController {
-	
+
 	@Autowired
 	private MenuItemService menuItemService;
-	
+
 	@Autowired
 	private RestaurantRepository restaurantRepository;
+
 	@PostMapping("/menus")
-	public ResponseEntity<Restaurant>addRestaurant(@RequestBody Restaurant restaurant){
-		Restaurant saved=restaurantRepository.save(restaurant);
-		return new ResponseEntity<>(saved,HttpStatus.CREATED);
+	public ResponseEntity<Restaurant> addRestaurant(@RequestBody Restaurant restaurant) {
+		Restaurant saved = restaurantRepository.save(restaurant);
+		return new ResponseEntity<>(saved, HttpStatus.CREATED);
 	}
-	
-	
+
 	@PostMapping("/{restaurantId}/menu")
-	public ResponseEntity<MenuItemResponseDTO>addMenuItem(@PathVariable int restaurantId, @Valid @RequestBody MenuItemRequestDTO request){
+	public ResponseEntity<MenuItemResponseDTO> addMenuItem(@PathVariable int restaurantId,
+			@Valid @RequestBody MenuItemRequestDTO request) {
 
-	MenuItemResponseDTO response =menuItemService.addMenuItem(restaurantId,request);
-	return new ResponseEntity<>(response, HttpStatus.CREATED);
-	
-	
+		MenuItemResponseDTO response = menuItemService.addMenuItem(restaurantId, request);
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+
 	}
-	
+
+	@GetMapping("/{restaurantId}/menu")
+	public ResponseEntity<List<CategoryMenuDto>> getMenu(@PathVariable int restaurantId) {
+
+		List<CategoryMenuDto> menu = menuItemService.getMenuByRestaurantId(restaurantId);
+
+		return new ResponseEntity<List<CategoryMenuDto>>(menu, HttpStatus.OK);
+	}
+
 }
-	
-
-
--- food-delivery-backend/src/main/java/com/ait/app/requestbody/MenuItemRequestDTO.java
+++ food-delivery-backend/src/main/java/com/ait/app/requestbody/MenuItemRequestDTO.java
@@ -1,50 +1,60 @@
 package com.ait.app.requestbody;
 
 public class MenuItemRequestDTO {
-	    
-	    private String name;
-	    private String description;
-	    private Double price;
-	    private Boolean availability;
-	    private String category;
-	    private int restaurantId;
-		public int getRestaurantId() {
-			return restaurantId;
-		}
-		public void setRestaurantId(int restaurantId) {
-			this.restaurantId = restaurantId;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getDescription() {
-			return description;
-		}
-		public void setDescription(String description) {
-			this.description = description;
-		}
-		public Double getPrice() {
-			return price;
-		}
-		public void setPrice(Double price) {
-			this.price = price;
-		}
-		public Boolean getAvailability() {
-			return availability;
-		}
-		public void setAvailability(Boolean availability) {
-			this.availability = availability;
-		}
-		public String getCategory() {
-			return category;
-		}
-		public void setCategory(String category) {
-			this.category = category;
-		}
+
+	private String name;
+	private String description;
+	private Double price;
+	private Boolean availability;
+	private String category;
+	private int restaurantId;
+
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Boolean getAvailability() {
+		return availability;
+	}
+
+	public void setAvailability(Boolean availability) {
+		this.availability = availability;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
 }
-
-
