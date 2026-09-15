ccc8cc6636937bd967bdfbf41641f8342b26234f
Saurabh More <saurabhmore2074@gmail.com>
2026-09-15 21:42:20 +0530

    ONL102
    Added AddressCustomException and Global Exception

--- food-delivery-backend/src/main/java/com/ait/app/globalexception/GlobalExceptionHandler.java
+++ food-delivery-backend/src/main/java/com/ait/app/globalexception/GlobalExceptionHandler.java
@@ -4,7 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+<<<<<<< Updated upstream
 import com.ait.app.exception.UserServiceCustomException;
+=======
+import com.ait.app.exception.AddressCustomException;
+import com.ait.app.exception.FeedbackCustomException;
+import com.ait.app.exception.UserServiceCustomException;
+import org.springframework.http.HttpStatus;
+>>>>>>> Stashed changes
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,6 +20,23 @@ public class GlobalExceptionHandler {
 	public ResponseEntity UserExceptionHandler(UserServiceCustomException e) {
 		return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
 	}
+<<<<<<< Updated upstream
+=======
+	
+	@ExceptionHandler(FeedbackCustomException.class)
+	public ResponseEntity<String> handleFeedbackException(
+	        FeedbackCustomException ex) {
+
+	    return new ResponseEntity<>(
+	            ex.getMessage(),
+	            org.springframework.http.HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(AddressCustomException.class)
+	public ResponseEntity<String> handleAddressException(AddressCustomException e) {
+	    return new ResponseEntity<>(e.getErrMsg(), e.getStatus());
+	}
+>>>>>>> Stashed changes
 
 
 }
