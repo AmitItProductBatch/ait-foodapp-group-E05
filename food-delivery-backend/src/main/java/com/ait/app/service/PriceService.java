8e2503ff5bf8c6ce9ed9a1e038475dd59faaeedf
Saurabh More <saurabhmore2074@gmail.com>
2026-09-19 12:18:12 +0530

    ONL115 Created View Item Price Story
    Created View Item price API with controller/service and PriceResponseDto

--- food-delivery-backend/src/main/java/com/ait/app/service/PriceService.java
+++ food-delivery-backend/src/main/java/com/ait/app/service/PriceService.java
@@ -1,9 +1,12 @@
 package com.ait.app.service;
 import com.ait.app.requestbody.PriceCalculationRequestDto;
 import com.ait.app.requestbody.PriceCalculationResponseDto;
+import com.ait.app.requestbody.PriceResponseDto;
 
 public interface PriceService {
 
 	PriceCalculationResponseDto calculatePrice(PriceCalculationRequestDto dto);
+	
+	PriceResponseDto getPrice(int itemId);
 
 }
