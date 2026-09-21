54dcbc69fbaf69438714d84d5015f8fd21fb075a
Saurabh More <saurabhmore2074@gmail.com>
2026-09-21 22:57:27 +0530

    ONL141 Restaurant Address Table Creation
    Created Restaurant Address table by implementing controller ,service and Dto's

--- food-delivery-backend/src/main/java/com/ait/app/model/Restaurant.java
+++ food-delivery-backend/src/main/java/com/ait/app/model/Restaurant.java
@@ -1,9 +1,8 @@
 package com.ait.app.model;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
---
+++ food-delivery-backend/src/main/java/com/ait/app/model/RestaurantAddress.java
@@ -0,0 +1,90 @@
+package com.ait.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "restaurant_address")
+public class RestaurantAddress {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private int houseNo;
+	private String streetName;
+	private String landmark;
+	private String city;
+	private int pinCode;
+	
+	@ManyToOne
+	@JoinColumn(name = "restaurant_id")
+	private Restaurant restaurant;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public String getLandmark() {
+		return landmark;
+	}
+
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	public int getHouseNo() {
+		return houseNo;
+	}
+
+	public void setHouseNo(int houseNo) {
+		this.houseNo = houseNo;
+	}
+	
+	
+	
+	
+	
+
+}
